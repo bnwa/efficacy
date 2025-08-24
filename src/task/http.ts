5d0cdda83fbbd84c6ad35fe1c690c2d728999a46
Benoit Barberousse <74591246+bnwa@users.noreply.github.com>
2025-08-24 12:57:33 -0500

    Fix task http retry err, impl progress when content-length present

--- src/task/http.ts
+++ src/task/http.ts
@@ -10,7 +10,7 @@ export function http(
     maxRetryMs = 30000,
     baseRetryMs = 1000,
     retryFactor = 2
-) {
+) : Task<Response, Pick<IO, "http">> {
   return Task.create(async function*(io: Pick<IO, 'http'>, signal?: AbortSignal) {
     let attempt = 0,
         msg = "",
@@ -20,23 +20,34 @@ export function http(
       try {
         if (signal?.aborted) return yield taskErr(false, String(signal.reason))
         const resp = await io.http(uri, { ...opts, signal })
-        if (resp.ok) return yield taskUpdate(resp, 1, 1)
+        if (resp.ok) {
+          const lengthVal = resp.headers.get('Content-Length')
+          if (lengthVal && resp.body) {
+            const maxBytes = parseInt(lengthVal, 10)
+            let byteSize = 0
+            // TODO Fix type issue
+            for await (const chunk of resp.body as any) {
+              byteSize += chunk.length
+              yield taskUpdate(resp, maxBytes / byteSize, 1)
+            }
+          }
+          return yield taskUpdate(resp, 1, 1)
+        }
+        msg = resp.statusText
       } catch(err) {
         if (err instanceof Error) msg = err.message
         else msg = String(err)
-      } finally {
-        attempt++
-        ms = Math.min(
-            baseRetryMs * Math.pow(retryFactor, attempt),
-            maxRetryMs
-        )
-        await new Promise(done => {
-          const tId = setTimeout(done, ms)
-          if (signal) {
-            signal.addEventListener('abort', () => clearTimeout(tId))
-          }
-        })
       }
+      ms = Math.min(
+        baseRetryMs * Math.pow(retryFactor, attempt++),
+        maxRetryMs
+      )
+      await new Promise(done => {
+        const tId = setTimeout(done, ms)
+        if (signal) {
+          signal.addEventListener('abort', () => clearTimeout(tId))
+        }
+      })
     }
 
     return yield taskErr(true, msg)
